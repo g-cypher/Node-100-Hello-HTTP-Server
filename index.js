c6232173b707335c7bd3bc4f48372b375acedabe
Garrett <cyph17@gmail.com>
2019-05-21 11:35:09 -0700

    splendiferous

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// write your code here
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World');
+  res.end();
+  }).listen(8080);
+  console.log("Listening now on port 8080");
+  
+  
+
+
+
